63b3eadc929104484bd171950650b0d993b1b153
luiszamudio4 <rodriguez.of.luis@gmail.com>
2019-07-11 21:35:36 -0500

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Pickles9)",
+    password: "rootroot",
     database: "burgers_db"
   });
 };
