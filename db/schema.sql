e6f4fdeec6c856d12d0ee04b57a995481189413e
luiszamudio4 <rodriguez.of.luis@gmail.com>
2019-07-11 21:18:12 -0500

    added database from mysql

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Drops the task_saver_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database task_saver_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table tasks.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(120) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
